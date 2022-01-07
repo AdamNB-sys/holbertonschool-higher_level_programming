c1b2ea1716119e24807437d4ceb205409f1567f3
AdamNB-sys <a1adnat@gmail.com>
2022-01-07 15:49:28 -0600

    hopefully have fixed vscode issues

--- 0x02-python-data_structures/0-print_list_integer.py
+++ 0x02-python-data_structures/0-print_list_integer.py
@@ -2,9 +2,5 @@
 
 
 def print_list_integer(my_list=[]):
-    for element in range(len(my_list)):
-<<<<<<< HEAD
-        print("{}".format(my_list[element]))
-=======
-        print("{:d}".format(my_list[element]))
->>>>>>> bd7aefef325b36d28570363de2dc19e09ec87895
+        for element in range(len(my_list)):
+                print("{:d}".format(my_list[element]))
