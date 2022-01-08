67bd8d1c2a9dc2bab29a45eedbe0195f0e40aa8a
AdamNB-sys <a1adnat@gmail.com>
2022-01-08 14:12:01 -0800

    resubmitting task 5

--- 0x02-python-data_structures/5-no_c.py
+++ 0x02-python-data_structures/5-no_c.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+
+
 def no_c(my_string):
-    newstring = my_string.translate({ord(c): None for c in "Cc"})
+    newstring = my_string.translate({ord(x): None for x in "Cc"})
     return newstring
