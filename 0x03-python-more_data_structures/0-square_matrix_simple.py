f336d9a5bbfa458d1237a1c78023f9500beb6107
AdamNB-sys <a1adnat@gmail.com>
2022-01-10 21:28:24 -0600

    tasks 0-6

--- 0x03-python-more_data_structures/0-square_matrix_simple.py
+++ 0x03-python-more_data_structures/0-square_matrix_simple.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
+
+
 def square_matrix_simple(matrix=[]):
+    new = []
+    for row in range(len(matrix)):
+        new.append(list(map(lambda i: i**2, matrix[row])))
+    return new
--- 0x03-python-more_data_structures/1-search_replace.py
+++ 0x03-python-more_data_structures/1-search_replace.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    newbie = []
+    for i in my_list:
+        if i == search:
+            i = newbie
+        newbie.append(i)
+    return newbie
