d1a70a848a3aea6956813f6ef89127e16c17c663
AdamNB-sys <a1adnat@gmail.com>
2022-01-10 23:44:16 -0600

    tasks 7-11

--- 0x03-python-more_data_structures/10-best_score.py
+++ 0x03-python-more_data_structures/10-best_score.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    if a_dictionary:
+        highScore = 0
+        for key, value in a_dictionary.items():
+            if value > highScore:
+                highScore, newkey = value, key
+        return newkey
