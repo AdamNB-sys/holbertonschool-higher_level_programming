e473a7bb1868760801288dee9e18e86455742062
AdamNB-sys <a1adnat@gmail.com>
2022-01-11 23:10:26 -0600

    tasks 0 and 1

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""This class defines the size of a square"""
+
+
+class Square:
+    """class square with a private size"""
+    def __init__(self, size):
+        self.__size = size
