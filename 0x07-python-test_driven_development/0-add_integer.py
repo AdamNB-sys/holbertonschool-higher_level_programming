a5465d276cb6b99d1246c2636c0d23bca456a1af
AdamNB-sys <a1adnat@gmail.com>
2022-01-15 13:33:22 -0600

    function to add integers. still lost on what else i'm supposed to do

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,7 +2,16 @@
 """
 add_integer:
     returns the sum of two ints or floats as an int
-"""
+    """
 
 
 def add_integer(a, b=98):
+    """
+    takes two numbers, either int or float, and adds them, 
+    returning an int
+    """
+    if type(a) is not int and type(a) is not float:
+        raise TypeError('a must be an integer')
+    if type(b) is not int and type(b) is not float:
+        raise TypeError('b must be integer')
+    return int(a + b)
