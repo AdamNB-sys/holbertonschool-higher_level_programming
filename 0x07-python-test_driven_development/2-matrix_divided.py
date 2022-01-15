c371e215fa898809be32c428722777ff45c83f8e
AdamNB-sys <a1adnat@gmail.com>
2022-01-15 18:37:28 -0500

    task 2 progress and test file

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -2,9 +2,22 @@
 """
 matrix_divided:
     Divides all values of a matrix
-    Returns a new matrix of integers rounded to two
-    decimal spaces
+    Returns a new matrix of integers
 """
 
 
 def matrix_divided(matrix, div):
+    """divides all elements of a matrix by div
+    checks for proper usage of matrix and div elements
+    """
+    matErr = 'matrix must be a matrix (list of lists) of integers/floats'
+    if type(div) is not int and type(div) is not float:
+        raise TypeError('div must be a number')
+
+    if type(matrix) is not list:
+        raise TypeError(matErr)
+
+    if div == 0:
+        raise ZeroDivisionError('division by zero')
+
+    
