adff941c1819fcca607739f6348ac4b6185255d5
AdamNB-sys <a1adnat@gmail.com>
2022-01-17 18:00:21 -0600

    fixed bad grammar and comment blocks

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
-"""
-add_integer:
-    returns the sum of two ints or floats as an int
-    """
+"""returns the sum of two ints or floats as an int"""
 
 
 def add_integer(a, b=98):
-    """
-    takes two numbers, either int or float, and adds them, 
-    returning an int
-    """
+    """adds int or float, returns an int. verifies types."""
     if type(a) is not int and type(a) is not float:
         raise TypeError('a must be an integer')
     if type(b) is not int and type(b) is not float:
-        raise TypeError('b must be integer')
+        raise TypeError('b must be an integer')
     return int(a) + int(b)
