16e1f047aa267cbaa1b26b2c105f25824427b1e8
AdamNB-sys <a1adnat@gmail.com>
2022-01-18 00:15:57 -0500

    whatever, this second deadline sucked anyway

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -4,4 +4,9 @@
 
 def text_indentation(text):
     """prints text with two newlines after each . ? :, and checks for usage"""
+    if type(text) is not str:
+        raise TypeError('text must be a string')
     
+    newliners = ['.', '?', ':']
+    for c in text:
+        
