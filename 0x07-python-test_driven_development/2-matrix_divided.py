2ba0d65468cf9a27c97977e5791192f3574b00f9
AdamNB-sys <a1adnat@gmail.com>
2022-01-17 21:45:25 -0600

    fixed commenting, added a test

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
-"""
-matrix_divided:
-    Divides all values of a matrix
-    Returns a new matrix of integers
-"""
-
+"""divides all values of a matrix and returns a new matrix"""
 
 
 def matrix_divided(matrix, div):
-    """divides all elements of a matrix by div
-    checks for proper usage of matrix and div elements
-    """
+    """use div on all elements and verifies usage"""
     matErr = 'matrix must be a matrix (list of lists) of integers/floats'
     if type(div) is not int and type(div) is not float:
         raise TypeError('div must be a number')
