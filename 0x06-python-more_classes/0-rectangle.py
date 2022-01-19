8c3ada55e2520a894dac0579997a4c2f323004a9
AdamNB-sys <a1adnat@gmail.com>
2022-01-19 17:27:10 -0600

    more error handling

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -5,8 +5,16 @@
 class Rectangle:
     """this defines the rectangle class"""
     def __init__(self, width=0, height=0):
-        self.__width = width
+        if type(width) is not int:
+            raise TypeError('width must be an integer')
+        if width < 0:
+            raise ValueError('width must be >= 0')
+        if type(height) is not int:
+            raise TypeError('height must be an integer')
+        if height < 0:
+            raise ValueError('height must be >= 0')
         self.__height = height
+        self.__width = width
 
     @property
     def width(self):
