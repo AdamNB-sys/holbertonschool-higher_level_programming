2680f5c19698d4c280c1855c2e8777e0b7483211
AdamNB-sys <a1adnat@gmail.com>
2022-01-20 01:49:19 -0500

    unittest cases for max_integer

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,14 +1,23 @@
+#!/usr/bin/python3
+"""unittest for max_integer([..])
+"""
 import unittest
-
 max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
 
-    def test_isempty(self):
-        """test for empty list"""
+    def test_values(self):
+        """test for proper evaluation"""
         self.assertEqual(max_integer([]), None)
+        self.assertEqual(max_integer([5]), 5)
+        self.assertEqual(max_integer([1, 2, 3]), 3)
+        self.assertEqual(max_integer([5, 10, 15]), 15)
+        self.assertEqual(max_integer([69, 420, 1312]), 1312)
+        self.assertEqual(max_integer([-10, -100, -1000]), -10)
 
-    def test
+    def test_none(self):
+        """tests for none"""
+        self.assertIsNone(max_integer([]))
 
 if __name__ == '__main__':
     unittest.main()
