99a97060490a0c0947e1ab1bd1d91d199c943661
AdamNB-sys <a1adnat@gmail.com>
2022-01-21 01:07:53 -0500

    i am in disbelief that these two tasks were this simple

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""checks to see if object is instance of class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """checks if object is instance"""
+
+    return (isinstance(obj, a_class))
