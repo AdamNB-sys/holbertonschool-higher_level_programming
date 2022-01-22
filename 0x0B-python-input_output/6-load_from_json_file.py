4442ac0034d5095189f7eae3c460e6f06114fff7
AdamNB-sys <a1adnat@gmail.com>
2022-01-22 00:32:17 -0500

    task 6, i'll do more over the weekend

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""method to create a Python object from a JSON file"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """converts JSON string to Python object"""
+
+    with open(filename) as f:
+        return json.loads(f.read())
