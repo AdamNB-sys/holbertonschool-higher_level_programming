d5bbef1f8ceeda0dec7ebb06eedc19bcf7c71c3d
AdamNB-sys <a1adnat@gmail.com>
2022-01-24 08:04:20 -0600

    task 7 finally

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""module adds all arguments to a Python list and saves them in a file"""
+
+
+import json
+import sys
+
+
+save_to = __import__('5-save_to_json_file').save_to_json_file
+load_from = __import__('6-load_from_json_file').load_from_json_file
+
+
+try:
+    new_list = load_from('add_item.json')
+except:
+    new_list = []
+
+for c in sys.argv[1:]:
+    new_list.append(c)
+
+save_to(new_list, "add_item.json")
