bf50223d3583b51caffcda57dca8ea1391d01577
AdamNB-sys <a1adnat@gmail.com>
2022-01-24 14:50:14 -0600

    task 8 files

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""method to convert class to JSON object"""
+
+
+import json
+
+
+def class_to_json(obj):
+    """returns the objects writable attributes"""
+    return obj.__dict__
