a403a33bc667bed641fb304032327e9c30c14329
AdamNB-sys <a1adnat@gmail.com>
2022-01-27 15:53:58 -0600

    getters, setters, and attribute checkers

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -15,4 +15,58 @@ class Rectangle(Base):
         self.__x = x
         self.__y = y
 
-    # def att_checker(self, blah blah blah )
+    @property
+    def width(self):
+        """returns width of the rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """sets width and validates usage"""
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        if value <= 0:
+            raise ValueError('width must be > 0')
+        self.__width = value
+
+    @property
+    def height(self):
+        """returns the height of the rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """"sets height and validates usage"""
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+        if value <= 0:
+            raise ValueError('height must be > 0')
+        self.__height = value
+
+    @property
+    def x(self):
+        """returns x, whatever that is"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """sets x and validates usage"""
+        if type(value) is not int:
+            raise TypeError('x must be an integer')
+        if value < 0:
+            raise ValueError('x must be >= 0')
+        self.__x = value
+
+    @property
+    def y(self):
+        """returns whatever y is"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """sets y and validates usage"""
+        if type(value) is not int:
+            raise TypeError('y must be an integer')
+        if value < 0:
+            raise ValueError('y must be >= 0')
+        self.__y = value
