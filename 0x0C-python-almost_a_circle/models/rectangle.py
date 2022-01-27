d073bdf28d6d436f0364eed60898e87c4be885e6
AdamNB-sys <a1adnat@gmail.com>
2022-01-27 16:24:00 -0600

    even more error handling!

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -9,10 +9,28 @@ class Rectangle(Base):
     """class for building rectangles with inheritance from Base"""
     def __init__(self, width, height, x=0, y=0, id=None):
         """initialization of class Rectangle"""
+        if type(id) is not int and id is not None:
+            raise TypeError('id must be an integer')
         super().__init__(id)
+        if type(width) is not int:
+            raise TypeError('width must be an integer')
+        if width <= 0:
+            raise ValueError('width must be greater than 0')
         self.__width = width
+        if type(height) is not int:
+            raise TypeError('height must be an integer')
+        if height <= 0:
+            raise ValueError('height must be > 0')
         self.__height = height
+        if type(x) is not int:
+            raise TypeError('x must be an integer')
+        if x < 0:
+            raise ValueError('x must be >= 0')
         self.__x = x
+        if type(y) is not int:
+            raise TypeError('y must be an integer')
+        if y < 0:
+            raise ValueError('y must be >= 0')
         self.__y = y
 
     @property
