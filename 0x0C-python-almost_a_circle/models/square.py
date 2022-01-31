aae8c673f6cc4d30fd288371fa7aa7af0c91ffaa
AdamNB-sys <a1adnat@gmail.com>
2022-01-31 11:04:11 -0600

    square str setter and getter, typo in rectangle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -8,3 +8,19 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """Class Square with inheritance from Rectangle"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """method to print square"""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x,
+                                                 self.y, self.width)
+
+    @property
+    def size(self):
+        """returns size of the square"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """sets width and validates usage"""
+        super().__init__(value, value)
