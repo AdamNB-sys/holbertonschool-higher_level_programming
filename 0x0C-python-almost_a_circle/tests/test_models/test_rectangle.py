99f6f0bfe29d38e8fad0be8433a9494bf068624a
AdamNB-sys <a1adnat@gmail.com>
2022-02-01 20:25:15 -0600

    more managable tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -7,9 +7,26 @@ import unittest
 from models.base import Base
 from models.rectangle import Rectangle
 from models import rectangle
-import inspect
-import pep8
+# import inspect
+# import pep8
 
 
 class TestRectangleDocs(unittest.TestCase):
     """Tests the Rectangle class' style and documentation"""
+
+    def test_module_docstring(self):
+        """Tests for the presence of a module docstring"""
+        self.assertTrue(len(rectangle.__doc__) >= 1)
+
+    def test_class_docstring(self):
+        """Tests for the presence of a class docstring"""
+        self.assertTrue(len(Rectangle.__doc__) >= 1)
+
+    def test_func_docstrings(self):
+        """Tests for the presence of docstrings in all functions"""
+        self.assertTrue(len(Rectangle.__init__.__doc__) >= 1)
+        self.assertTrue(len(Rectangle.to_dictionary.__doc__) >= 1)
+        self.assertTrue(len(Rectangle.__str__.__doc__) >= 1)
+        self.assertTrue(len(Rectangle.update.__doc__) >= 1)
+        self.assertTrue(len(Rectangle.display.__doc__) >= 1)
+        self.assertTrue(len(Rectangle.area.__doc__) >= 1)
--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """Unittesting for the Square module/class
 Tests are done for each method of the class"""
-import pep8
+
+
+# import pep8
 import unittest
 from models.base import Base
 from models.rectangle import Rectangle
@@ -10,3 +12,38 @@ from models.square import Square
 
 class TestClassSquare(unittest.TestCase):
     """Test class for testing Square class"""
+
+    def test_id(self):
+        """
+        Test that the id of the square is correct
+        """
+        s1 = Square(10)
+        self.assertEqual(s1.id, 1)
+        s2 = Square(10, 0, 0, 12)
+        self.assertEqual(s2.id, 12)
+
+    def test_width(self):
+        """
+        Test that the width of the square is correct
+        """
+        s1 = Square(10)
+        self.assertEqual(s1.width, 10)
+        s2 = Square(10, 0, 0, 12)
+
+    def test_height(self):
+        """
+        Test that the height of the square is correct
+        """
+        s1 = Square(10)
+        self.assertEqual(s1.height, 10)
+        s2 = Square(10, 0, 0, 12)
+        self.assertEqual(s2.height, 10)
+
+    def test_x(self):
+        """
+        Test that the x of the square is correct
+        """
+        s1 = Square(10)
+        self.assertEqual(s1.x, 0)
+        s2 = Square(10, 0, 0, 12)
+        self.assertEqual(s2.x, 0)
