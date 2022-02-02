d545c7294164fc6f79a34f529a77cab74af89926
AdamNB-sys <a1adnat@gmail.com>
2022-02-01 20:15:40 -0600

    updated tests, cannot test pep8 locally

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -3,22 +3,37 @@
 Tests are done for each method of the class"""
 
 
-# import pep8
-import unittest
+import inspect
 from models.base import Base
 from models.rectangle import Rectangle
 from models.square import Square
+import unittest
+import pep8
+import json
+import os
 
 
-class TestClassRectangle(unittest.TestCase):
-    """Test class for testing Rectangle class"""
-    def test_pep8_rectangle(self):
+class TestBaseClass(unittest.TestCase):
+    """
+    Test Base Class
+    """
+
+    def test_pep8_base(self):
         """
-        Test that models/rectanlge.py is pep8 compliant.
+        Test that models/base.py is pep8 compliant.
         """
         pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['models/rectangle.py'])
-        self.assertEqual(result.total_errors, 0, 
+        result = pep8style.check_files(['models/base.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_pep8_test_base(self):
+        """
+        Test that tests/test_models/test_base.py is pep8 compliant
+        """
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['tests/test_models/test_base.py'])
+        self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
     def test_module_docstring(self):
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -3,20 +3,13 @@
 Tests are done for each method of the class"""
 
 
-# import pep8
 import unittest
 from models.base import Base
 from models.rectangle import Rectangle
-from models.square import Square
+from models import rectangle
+import inspect
+import pep8
 
 
-class TestClassRectangle(unittest.TestCase):
-    """Test class for testing Rectangle class"""
-    def test_pep8_rectangle(self):
-        """
-        Test that models/rectanlge.py is pep8 compliant.
-        """
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['models/rectangle.py'])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warnings).")
+class TestRectangleDocs(unittest.TestCase):
+    """Tests the Rectangle class' style and documentation"""
