d8b2459a41ed88b2017f54018010931b2a8b477e
AdamNB-sys <a1adnat@gmail.com>
2022-02-15 18:45:57 -0600

    tasks 10 and 11

--- 0x01-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x01-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,5 @@
 -- Lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
 -- Import database dump hbtn_0d_tvshows from external source (provided in task)
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
--- 0x01-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x01-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,3 +1,7 @@
 -- Lists all shows contained in the database hbtn_0d_tvshows
 -- Record display: tv_shows.title - tv_show_genres.genre_id
 -- Sorted in ascending order by tv_shows.title and tv_shows_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows LEFT OUTER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
