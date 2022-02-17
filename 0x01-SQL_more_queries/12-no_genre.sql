0640a960065be863e96fa5f537daff81bb836b29
AdamNB-sys <a1adnat@gmail.com>
2022-02-16 19:14:51 -0600

    some progress

--- 0x01-SQL_more_queries/12-no_genre.sql
+++ 0x01-SQL_more_queries/12-no_genre.sql
@@ -1,3 +1,5 @@
 -- Lists all shows in hbtn_0d_tvshows that have no genre linked
 -- Record display: tv_shows.title - tv_shows_genres.genre_id
 -- Sorted in ascending order by tv_shows.title and tv_shows_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows LEFT JOIN tv_show_genres
