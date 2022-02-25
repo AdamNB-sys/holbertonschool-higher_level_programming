7371e9a4f7669badda74ebb069a8417c6d63e071
AdamNB-sys <a1adnat@gmail.com>
2022-02-24 19:30:47 -0600

    files plus test

---
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""lists all states from hbtn_0e_0_usa database"""
+
+import MySQLdb
+from sys import argv
+
+
+def print_state():
+    db = MySQLdb.connect(
+        host='localhost', user=argv[1],
+        passwd=argv[2], db=argv[3], port=3306)
+
+    cur = db.cursor()
+    cur.execute("SELECT id, name FROM states ORDER BY id ASC")
+    for rows in cur.fetchall():
+        print(rows)
+
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    print_state()
---
+++ 0x0B-python-object_relational_mapping/README.md
@@ -0,0 +1,4 @@
+0x0B Python - Object-relational mapping
+
+In this project we are dealing with a concept that is so controvertial in 
+the programming world that it was referred to as the Vietnam of computer programming.
