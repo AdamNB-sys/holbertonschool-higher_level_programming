3fa135d288f4c96b0b287c05d4cb14e19afc8456
AdamNB-sys <a1adnat@gmail.com>
2022-02-24 22:00:08 -0600

    some changes

--- 0x0B-python-object_relational_mapping/0-select_states.py
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """lists all states from hbtn_0e_0_usa database"""
-import MySQLdb
-from sys import argv
 
 
-def main():
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
     db = MySQLdb.connect(
-        host='localhost', port=3306, user=argv[1],
-        passwd=argv[2], db=argv[3])
+        host='localhost', port=3306, user=argv[1], passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
     cur.execute("SELECT id, name FROM states ORDER BY id ASC")
@@ -17,7 +17,3 @@ def main():
 
     cur.close()
     db.close()
-
-
-if __name__ == "__main__":
-    main()
