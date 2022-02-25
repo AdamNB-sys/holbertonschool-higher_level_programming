9de8f328e2f3de936733efc893996677f49de19d
AdamNB-sys <a1adnat@gmail.com>
2022-02-25 11:01:29 -0600

    where name like binary

--- 0x0B-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0B-python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""takes an argument and returns matching results from
+the hbtn_0e_0_usa database"""
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(
+        host='localhost', port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name\
+        LIKE binary %s".format(argv[4]))
+    states = cur.fetchall()
+    for row in states:
+        print(row)
+
+    cur.close()
+    db.close()
