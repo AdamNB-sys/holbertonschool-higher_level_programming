20810628107c4ea5451b79033befe0f0097294a9
AdamNB-sys <a1adnat@gmail.com>
2022-02-25 20:42:35 -0600

    formatting i think

--- 0x0B-python-object_relational_mapping/model_state.py
+++ 0x0B-python-object_relational_mapping/model_state.py
@@ -3,12 +3,11 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-
 Base = declarative_base()
 
 
 class SomeClass(Base):
-    """this is the class states"""
+    """Creation of states class"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
