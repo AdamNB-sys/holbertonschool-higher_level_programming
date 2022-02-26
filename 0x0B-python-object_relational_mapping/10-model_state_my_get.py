e1a4611485f21352c833aca2e7dda84b8a25fcac
AdamNB-sys <a1adnat@gmail.com>
2022-02-25 23:21:59 -0600

    tasks 9 and 10

--- 0x0B-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0B-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""lists first State object in the hbtn_0e_6_usa database"""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = sessionmaker(bind=engine)
+    Session = session()
+    state_id = Session.query(State).filter(State.name == argv[4]).first()
+
+    if state_id:
+        print(state_id.id)
+    else:
+        print('Not found')
+    Session.close()
--- 0x0B-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0B-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""lists all State objects from the hbtn_0e_6_usa database
+with an 'a' in their name"""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = sessionmaker(bind=engine)
+    Session = session()
+    for state in Session.query(State).order_by(State.id.asc()).all():
+        if 'a' in state.name:
+            print("{}: {}".format(state.id, state.name))
+    Session.close()
