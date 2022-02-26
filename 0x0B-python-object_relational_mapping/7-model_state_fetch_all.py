8c2c0ab1af0fc4777b6338c1403c89a9eff3a095
AdamNB-sys <a1adnat@gmail.com>
2022-02-25 21:48:32 -0600

    current progress

--- 0x0B-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0B-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""lists all State objects from the hbtn_0e_6_usa database"""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
