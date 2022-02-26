e3dc9554f082f10a4a1868f8210dbc39c7519eb0
AdamNB-sys <a1adnat@gmail.com>
2022-02-25 20:36:36 -0600

    i think this will work

---
+++ 0x0B-python-object_relational_mapping/model_state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""class state with inheritance from base"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class SomeClass(Base):
+    """this is the class states"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
