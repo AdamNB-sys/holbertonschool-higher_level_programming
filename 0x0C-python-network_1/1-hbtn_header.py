06495fa2907ab3dd65f890d14a95dda5b4564e83
AdamNB-sys <a1adnat@gmail.com>
2022-03-08 20:07:19 -0600

    committing progress for task 1, continue on laptop

--- 0x0C-python-network_1/1-hbtn_header.py
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""Requests URL and displays value of X-Request-Id"""
+
+
+import urllib.request
+import sys
+
+
+if __name__ == "__main__":
+    """Method to fetch and return request id"""
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        header = response.info
