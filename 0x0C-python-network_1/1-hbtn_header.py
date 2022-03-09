5201e047ce35bf829f1db0cce14a3bac0de6603a
AdamNB-sys <a1adnat@gmail.com>
2022-03-08 20:46:59 -0600

    unable to test locally, comitting to test on vm

--- 0x0C-python-network_1/1-hbtn_header.py
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -9,4 +9,5 @@ import sys
 if __name__ == "__main__":
     """Method to fetch and return request id"""
     with urllib.request.urlopen(sys.argv[1]) as response:
-        header = response.info
+        header = response.headers.get('X-Request-Id')
+        print(header)
