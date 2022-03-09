945144b7f9346949813d66fcacfdb19f2d282006
AdamNB-sys <a1adnat@gmail.com>
2022-03-08 19:49:09 -0600

    task 0 and exe'ing files

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""Fetches a url using urllib"""
+
+
+import urllib.request
+
+
+if __name__ == "__main__":
+    """method to fetch url and print status"""
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf-8 content: {}'.format(html.decode('utf-8')))
