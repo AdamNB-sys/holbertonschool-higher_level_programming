277522ed432c923fc771b50ca73bc002b0a99d3d
AdamNB-sys <a1adnat@gmail.com>
2022-03-10 14:41:51 -0600

    task 4, it was easier than the first task, go figure

--- 0x0C-python-network_1/4-hbtn_status.py
+++ 0x0C-python-network_1/4-hbtn_status.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""Fetches url using requests"""
+
+
+if __name__ == "__main__":
+    """Gets url"""
+    from requests import get
+
+    url = get('https://intranet.hbtn.io/status')
+    html = url.text
+    print('Body response:')
+    print('\t- type: {}'.format(type(html)))
+    print('\t- content: {}'.format(html))
