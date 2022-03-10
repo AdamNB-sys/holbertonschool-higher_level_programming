40e5ce91321f18ae84d8952123adff29ffdbc6c4
AdamNB-sys <a1adnat@gmail.com>
2022-03-10 13:24:21 -0600

    make a post request, thanks tim!

--- 0x0C-python-network_1/2-post_email.py
+++ 0x0C-python-network_1/2-post_email.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""Sends POST request to URL in the form of an email"""
+
+
+from urllib.request import Request, urlopen
+from urllib.parse import urlencode
+import sys
+
+
+if __name__ == "__main__":
+    """Method to fetch and return request id"""
+    email = {'email': sys.argv[2]}
+    payload = urlencode(email).encode('utf-8')
+    query = Request(sys.argv[1], payload)
+    with urlopen(query) as response:
+        print(response.read().decode('utf-8'))
