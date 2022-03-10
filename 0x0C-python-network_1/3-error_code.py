3f9598b55c853bf7eb042f47a25dfd84cb49c2ca
AdamNB-sys <a1adnat@gmail.com>
2022-03-10 14:21:45 -0600

    task 3, pings url, returns error code

--- 0x0C-python-network_1/3-error_code.py
+++ 0x0C-python-network_1/3-error_code.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""Sends request to url and returns correct error code"""
+
+
+if __name__ == "__main__":
+    """Requests url and returns error codes"""
+    from urllib.request import Request, urlopen
+    from urllib.error import HTTPError
+    import sys
+
+    url = Request(sys.argv[1])
+    try:
+        with urlopen(url) as response:
+            print(response.read().decode('utf-8'))
+    except HTTPError as error:
+        print('Error code: {}'.format(error.code))
