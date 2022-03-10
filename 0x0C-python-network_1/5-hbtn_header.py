f81f222fedda8d711426c7aa1b241774631c1e30
AdamNB-sys <a1adnat@gmail.com>
2022-03-10 14:57:16 -0600

    get x-request-id

--- 0x0C-python-network_1/5-hbtn_header.py
+++ 0x0C-python-network_1/5-hbtn_header.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""Uses requests to ping a url and display X-Request-Id"""
+
+
+if __name__ == "__main__":
+    """Displays content of X-Request-Id"""
+    from requests import get
+    import sys
+
+    url = get(sys.argv[1])
+    header = url.headers.get('X-Request-Id')
+    print(header)
