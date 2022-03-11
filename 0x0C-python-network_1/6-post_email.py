8db1c375514b066b67da3ad354eebc1dff11f6bb
AdamNB-sys <a1adnat@gmail.com>
2022-03-10 21:33:26 -0600

    post an email

--- 0x0C-python-network_1/6-post_email.py
+++ 0x0C-python-network_1/6-post_email.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""Sends email as post request"""
+
+
+if __name__ == "__main__":
+    """Uses the requests post method to send an email to a server"""
+    from requests import post
+    import sys
+
+    email = {'email': sys.argv[2]}
+    post_req = post(sys.argv[1], email)
+    print(post_req.text)
