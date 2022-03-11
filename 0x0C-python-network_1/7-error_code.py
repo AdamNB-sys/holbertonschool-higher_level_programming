9aec57744d53859a02e0d58956c93be1afaaa85b
AdamNB-sys <a1adnat@gmail.com>
2022-03-10 21:52:00 -0600

    get status from ip

--- 0x0C-python-network_1/7-error_code.py
+++ 0x0C-python-network_1/7-error_code.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""Pings a url and displays the response as an error code"""
+
+
+if __name__ == "__main__":
+    """Uses the requests get method"""
+    from requests import get
+    import sys
+
+    status = get(sys.argv[1])
+    if status.status_code >= 400:
+        print('Error code: {}'.format(status.status_code))
+    else:
+        print(status.text)
