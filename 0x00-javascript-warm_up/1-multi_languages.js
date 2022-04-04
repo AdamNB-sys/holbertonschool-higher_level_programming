1e29f7c43845f7996fe1014ce9cf0cf7a07d2bb6
AdamNB-sys <a1adnat@gmail.com>
2022-04-04 16:42:01 -0500

    removed extra newline

--- 0x00-javascript-warm_up/1-multi_languages.js
+++ 0x00-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 // prints 3 lines
-const propaganda = 'C is fun\nPython is cool\nJavaScript is amazing\n';
+const propaganda = 'C is fun\nPython is cool\nJavaScript is amazing';
 console.log(propaganda);
