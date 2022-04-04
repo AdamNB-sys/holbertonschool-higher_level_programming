aeb9814669b382ca950179b007ac11ec69b33241
AdamNB-sys <a1adnat@gmail.com>
2022-04-04 18:21:55 -0500

    javascript handles args weird

--- 0x00-javascript-warm_up/2-arguments.js
+++ 0x00-javascript-warm_up/2-arguments.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+// prints a message based on number of args passed
+const args = process.argv.length
+
+if (args <= 2) {
+    console.log('No argument')
+} else if (args === 3) {
+    console.log('Argument found')
+} else {
+    console.log('Arguments found')
+}
+
+/*if (process.argv.length <= 3) {
+    console.log('Argument found')
+}
+console.log(process.argv[0])
+console.log(process.argv[1])
+console.log(process.argv[2])
+console.log(process.argv[3])*/
