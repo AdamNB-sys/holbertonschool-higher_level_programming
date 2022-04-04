45d1e5c53b184cac23235d7da6cd3e1da56ca983
AdamNB-sys <a1adnat@gmail.com>
2022-04-04 18:24:39 -0500

    fixed tabs and semicolons

--- 0x00-javascript-warm_up/2-arguments.js
+++ 0x00-javascript-warm_up/2-arguments.js
@@ -1,19 +1,11 @@
 #!/usr/bin/node
 // prints a message based on number of args passed
-const args = process.argv.length
+const args = process.argv.length;
 
 if (args <= 2) {
-    console.log('No argument')
+  console.log('No argument');
 } else if (args === 3) {
-    console.log('Argument found')
+  console.log('Argument found');
 } else {
-    console.log('Arguments found')
+  console.log('Arguments found');
 }
-
-/*if (process.argv.length <= 3) {
-    console.log('Argument found')
-}
-console.log(process.argv[0])
-console.log(process.argv[1])
-console.log(process.argv[2])
-console.log(process.argv[3])*/
