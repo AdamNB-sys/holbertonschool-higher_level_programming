cd46ada7a59d8037d98305ee0cd9c40a8b01f45c
AdamNB-sys <a1adnat@gmail.com>
2022-04-04 20:29:39 -0500

    better square method to match example

--- 0x00-javascript-warm_up/8-square.js
+++ 0x00-javascript-warm_up/8-square.js
@@ -1,15 +1,12 @@
 #!/usr/bin/node
 const size = process.argv[2];
-let square = '';
+// let square = '';
 
 if (Number(size)) {
   for (let x = 0; x < size; x++) {
-    for (let y = 0; y < size; y++) {
-      square += 'X';
-    }
-    square += '\n';
+    console.log('X'.repeat(size));
   }
-  console.log(square);
+  // console.log(square);
 } else {
   console.log('Missing number of occurrences');
 }
