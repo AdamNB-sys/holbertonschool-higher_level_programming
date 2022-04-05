7c8cff6236dc3fd67d5f4d55dacca7fb42b7cd7d
AdamNB-sys <a1adnat@gmail.com>
2022-04-04 20:22:12 -0500

    making squares

--- 0x00-javascript-warm_up/8-square.js
+++ 0x00-javascript-warm_up/8-square.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+const size = process.argv[2];
+let square = "";
+
+if (Number(size)) {
+  for (let x = 0; x < size; x++) {
+    for (let y = 0; y < size; y++) {
+      square += "X";
+    }
+    square += '\n';
+  }
+  console.log(square);
+} else {
+  console.log('Missing number of occurrences');
+}
