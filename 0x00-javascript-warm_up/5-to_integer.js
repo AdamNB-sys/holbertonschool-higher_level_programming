737bdafd70417c23272db7c14bd7a1e401e97fcf
AdamNB-sys <a1adnat@gmail.com>
2022-04-04 19:21:52 -0500

    2 space tabs instead of 4 spaces

--- 0x00-javascript-warm_up/5-to_integer.js
+++ 0x00-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // prints arg to stdout if it is a number
 if (Number(process.argv[2])) {
-    console.log(`My number: ${process.argv[2]}`);
+  console.log(`My number: ${process.argv[2]}`);
 } else {
-    console.log('Not a number');
+  console.log('Not a number');
 }
