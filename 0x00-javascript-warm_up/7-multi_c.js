b771eda4b805b131fc39235d627520a086907cf0
AdamNB-sys <a1adnat@gmail.com>
2022-04-04 20:05:18 -0500

    multi C

--- 0x00-javascript-warm_up/7-multi_c.js
+++ 0x00-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const x = process.argv
+
+if (Number(x[2])) {
+  for (let i = 0; i < x[2]; i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
