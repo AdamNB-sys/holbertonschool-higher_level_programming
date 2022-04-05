947b6f25dd6260318df44f0ccb1948ffa8c17e21
AdamNB-sys <a1adnat@gmail.com>
2022-04-04 20:52:32 -0500

    should really run semistandard first

--- 0x00-javascript-warm_up/9-add.js
+++ 0x00-javascript-warm_up/9-add.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-function add(a, b) {
-  let sum = a + b;
+function add (a, b) {
+  const sum = a + b;
   return sum;
 }
-console.log(add(Number(process.argv[2]), Number(process.argv[3])))
+console.log(add(Number(process.argv[2]), Number(process.argv[3])));
