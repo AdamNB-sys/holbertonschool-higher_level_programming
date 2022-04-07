f5d78718c4d3fffa8b480c44f05d425a25906ec1
AdamNB-sys <a1adnat@gmail.com>
2022-04-07 18:12:39 -0500

    get contents, store to file

--- 0x02-javascript-web_scraping/5-request_store.js
+++ 0x02-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+const fileWriter = fs.createWriteStream(process.argv[3]);
+
+request(process.argv[2], { json: true }, (err, res, body) => {
+  if (err) { return console.log(err); }
+  // console.log(body);
+  fileWriter.write(body, 'utf-8');
+});
