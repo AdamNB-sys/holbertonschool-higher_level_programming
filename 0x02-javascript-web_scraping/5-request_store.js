696a66dba31278f2a60c661838e68b31016d2e72
AdamNB-sys <a1adnat@gmail.com>
2022-04-07 18:39:53 -0500

    changed logic to if/else to catch errors

--- 0x02-javascript-web_scraping/5-request_store.js
+++ 0x02-javascript-web_scraping/5-request_store.js
@@ -4,10 +4,11 @@ const fs = require('fs');
 // const fileWriter = fs.createWriteStream(process.argv[3]);
 
 request(process.argv[2], { json: true }, (err, res, body) => {
-  if (err) { return console.log(err); }
-  // console.log(body);
-  // fileWriter.write(body, 'utf-8');
-  fs.writeFile(process.argv[3], body, 'utf-8', err => {
-    if (err) { return console.log(err); }
-  });
+  if (err) {
+    return console.log(err);
+  } else {
+    fs.writeFile(process.argv[3], body, 'utf-8', err => {
+      if (err) { return console.log(err); }
+    });
+  }
 });
