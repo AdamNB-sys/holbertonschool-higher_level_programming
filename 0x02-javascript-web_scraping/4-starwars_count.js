0abfe82edcd80f42815c083a8ff8e835a621c09a
AdamNB-sys <a1adnat@gmail.com>
2022-04-07 17:46:23 -0500

    find our Wedge!

--- 0x02-javascript-web_scraping/4-starwars_count.js
+++ 0x02-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,21 @@
 #!/usr/bin/node
+const request = require('request');
+
+request(process.argv[2], { json: true }, (err, res, body) => {
+  if (err) { return console.log(err); }
+  // console.log(body.results);
+  const response = (body.results);
+  // console.log(response);
+  let wedgeCount = 0;
+  for (const movies in response) {
+    const characters = response[movies].characters;
+    // console.log(characters);
+    for (const Wedge in characters) {
+      if (characters[Wedge].includes('/18/')) {
+        // console.log(`Wedge found! : ${characters[Wedge]}`);
+        wedgeCount += 1;
+      }
+    }
+  }
+  console.log(wedgeCount);
+});
