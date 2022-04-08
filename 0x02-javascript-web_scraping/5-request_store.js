ae8017a659fd9dd0161544a654d6430be942ef82
AdamNB-sys <a1adnat@gmail.com>
2022-04-07 19:24:35 -0500

    try wrapping err in ()

--- 0x02-javascript-web_scraping/5-request_store.js
+++ 0x02-javascript-web_scraping/5-request_store.js
@@ -7,7 +7,7 @@ request(process.argv[2], { json: true }, (err, res, body) => {
   if (err) {
     return console.log(err);
   } else {
-    fs.writeFile(process.argv[3], body, 'utf-8', err => {
+    fs.writeFile(process.argv[3], body, 'utf-8', (err) => {
       if (err) { return console.log(err); }
     });
   }
